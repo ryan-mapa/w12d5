6ae88008492f6ceaf571a6f09f5491741a78c7e7
ryan mapa <ryan.m.mapa@gmail.com>
2017-10-22 21:53:46 -0700

    Finish bst practical

--- ryan_mapa/lib/bst_practical.rb
+++ ryan_mapa/lib/bst_practical.rb
@@ -1,5 +1,7 @@
 require_relative 'binary_search_tree.rb'
 
 def kth_largest(tree_node, k)
-  in_order_traversal(tree_node)[-k]
+  tree = BinarySearchTree.new
+  val = tree.in_order_traversal(tree_node)[-k]
+  tree.find(val, tree_node)
 end
